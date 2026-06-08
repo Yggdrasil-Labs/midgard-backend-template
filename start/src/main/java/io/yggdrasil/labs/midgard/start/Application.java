0d41a5bc264e67e8fb1149c79658f345f881fea0
YangYang <1059376437@qq.com>
2026-06-08 08:56:04 +0800

    fix(start): 添加 @MapperScan 扫描 AutoMybatis 生成的 Mapper

--- start/src/main/java/io/yggdrasil/labs/midgard/start/Application.java
+++ start/src/main/java/io/yggdrasil/labs/midgard/start/Application.java
@@ -1,5 +1,6 @@
 package io.yggdrasil.labs.midgard.start;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author YoungerYang-Y
  */
 @SpringBootApplication(scanBasePackages = {"io.yggdrasil.labs.midgard", "com.alibaba.cola"})
+@MapperScan("io.yggdrasil.labs.midgard.infrastructure.persistence.dataobject.mapper")
 public class Application {
 
     public static void main(String[] args) {
